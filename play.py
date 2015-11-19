7ab90c43f1f98dc1bfa7fbabf0e036806722ef59
JulV <julvdev@gmail.com>
2015-11-19 21:44:31 +0100

    all in english

--- play.py
+++ play.py
@@ -1,9 +1,11 @@
 from interface import *
+from core import *
+
 
 def play():
     print_msg_start()
     print("Rest of the code here")
 
 
-if __name__ == "__main__":  # Si le fichier n'est pas un module, qu'il est appelé seul (en main)
-    play()  # Lancer le jeu
+if __name__ == "__main__":  # If it's not a module, if it's called (main)
+    play()  # Launch the game
