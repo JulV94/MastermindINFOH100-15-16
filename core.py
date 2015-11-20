2da0c0506285d12617c430579ada0155e83a9789
JulV <julvdev@gmail.com>
2015-11-20 15:36:10 +0100

    is_valid_pattern()+try_pattern()

--- core.py
+++ core.py
@@ -2,9 +2,28 @@ def random_code():
     pass
 
 
-def is_valid_pattern(pattern):
-    pass
+def is_valid_pattern(pattern):  # Ugly code but we have to put only one return -_-
+    result = True
+    valid_colors = ['r', 'j', 'v', 'b', 'o', 'c', 't', 'f']
+    if len(pattern) != 4:
+        result = False
+    for elem in pattern:
+        if elem not in valid_colors:
+            result = False
+    return result
 
 
 def try_pattern(pattern, code):
-    pass
+    well_placed = 0
+    good_color = 0
+    for i in range(len(pattern)-1, -1, -1):
+        if pattern[i] == code[i]:
+            well_placed += 1
+            pattern.pop(i)
+            code.pop(i)
+
+    for elem in pattern:
+        if elem in code:
+            good_color += 1
+
+    return tuple(well_placed, good_color)
